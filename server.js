2f82d5f89cfd7019f4c4f1998d9d9aad7f234b5e
Phalen Hazel <88113752+PhalenH@users.noreply.github.com>
2021-11-12 22:50:36 -0500

    added code to access middleware

--- server.js
+++ server.js
@@ -1,7 +1,27 @@
+// importing express and fs packages
 const express = require('express');
 const fs = require('fs');
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
-// TODO - make public folder static
+
+// Middleware for parsing application/json and urlencoded data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static('public'));
+
+
+
+
+
+
+
+
+
+
+
+app.listen(PORT, () =>
+  console.log(`Note taker listening at http://localhost:${PORT}`)
+);
