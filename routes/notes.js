e9acf8d2d89e076312ce075e030891b46eec89f2
Phalen Hazel <88113752+PhalenH@users.noreply.github.com>
2021-11-14 14:43:10 -0500

    added and started get/post routes in notes.js, ongoing

--- routes/notes.js
+++ routes/notes.js
@@ -9,5 +9,18 @@ notes.get("/test", (req, res) => {
   res.send("notes works");
 });
 
+// GET /api/notes should read the db.json file and return all saved notes as JSON.
+notes.get("/api/notes", (req, res) => {
+  console.log("notes api works");
+  res.send("notes api works");
+});
+
+// POST /api/notes should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client.
+notes.post("/api/notes", (req, res) =>{
+    
+})
+// You'll need to find a way to give each note a !! unique id !! when it's saved
+// (look into npm packages that could do this for you)
+
 // export route
 module.exports = notes;
